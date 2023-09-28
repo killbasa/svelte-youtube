ff8482d296d73fe9419042971a73b88b90cb0c81
killbasa <killbasab@gmail.com>
2023-09-27 20:28:41 -0400

    fix: exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
 export { default as YoutubePlayer } from './YoutubePlayer.svelte';
 export * from './types/events';
+export * from './types/generic';
 export * from './types/media';
+export * from './Embed';
+export * from './PlayerHandler';
